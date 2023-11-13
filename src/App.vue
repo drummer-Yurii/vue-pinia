e719409fcdbbc06e167e7b01c5bad69cca22ec1c
Yurii Rybachok <drummer2009@ukr.net>
2023-11-13 09:08:37 +0200

    add data & logic for create user

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,27 @@
 <script setup>
+import { ref } from 'vue';
+import { useUserStore } from './stores/users';
+
+const user_store = useUserStore();
+
+const user_input = ref({
+  name: '',
+  email: '',
+});
+
+const sort = ref(false);
+
+const CreateUser = () => {
+  if (!user_input.value.name || !user_input.value.email) {
+    return alert('Please enter both name and email')
+  }
+  user_store.createUser(user_input.value)
+
+  user_input.value = {
+    name: '',
+    email: '',
+  }
+}
 </script>
 
 <template>
